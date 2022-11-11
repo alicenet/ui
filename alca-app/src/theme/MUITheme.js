1236b269d1e7da4fda08b2970438cf0634269f1a
Everton Renan <everton.renan@clevertech.biz>
2022-11-11 12:28:29 -0300

    feat: add default colors

--- alca-app/src/theme/MUITheme.js
+++ alca-app/src/theme/MUITheme.js
@@ -1,21 +1,50 @@
 import { createTheme } from "@mui/material";
 import { theme as aliceTheme } from "alice-mui-provider";
 
-console.log({ aliceTheme });
-
-const CustomColors = {
-    background: "#1E1E1E",
-    copperCrayola: "#ea9167",
-    darkPurple: "#24031",
-    midnightBlue: "#1d0a66",
-    midnight: "#7e176b",
-    thistle: "#e3c8e2",
+const CustomPaletteColors = {
+    mode: "dark",
+    primary: {
+        main: "#95F1E3",
+        light: "#C9FFFF",
+    },
+    secondary: {
+        main: "#FFABD1",
+        dark: "#B27792",
+    },
+    headerBlack: {
+        main: "#171717",
+    },
+    dark: {
+        main: "#212121",
+        light: "#0000000A",
+    },
+    darkGray: {
+        main: "#2D2D2D",
+    },
+    rowBlack: {
+        main: "#343333",
+    },
+    tableBlack: {
+        main: "#444444",
+    },
+    buttonBlack: {
+        main: "#494949",
+    },
+    lightGray: {
+        main: "#A7A6A6",
+    },
+    clearGray: {
+        main: "#DEDEDE",
+    },
+    dropGray: {
+        main: "#EEEEEE",
+    },
+    background: {
+        default: "#11151C",
+    },
 };
 
 export const theme = createTheme({
     ...aliceTheme,
-    palette: {
-        ...aliceTheme.palette,
-        background: { default: CustomColors.background },
-    },
+    palette: CustomPaletteColors,
 });
