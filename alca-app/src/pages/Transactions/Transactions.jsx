d3d896e3e00a33accfe6c9f30bc6c8d267313a5e
Everton Renan <everton.renan@clevertech.biz>
2022-11-17 17:01:21 -0300

    chore: clean up styles

--- alca-app/src/pages/Transactions/Transactions.jsx
+++ alca-app/src/pages/Transactions/Transactions.jsx
@@ -23,26 +23,18 @@ export function Transactions() {
             167.76%
         )`,
         color: "secondary.contrastText",
-        borderRadius: "4px",
     };
-    const columnOneTitleBoxSx = sx({ p: 2 }, { condition: alca <= 0, sx: activeBoxTitleStyles });
-    const columnTwoTitleBoxSx = sx({ p: 2 }, { condition: alca > 0, sx: activeBoxTitleStyles });
+    const columnOneTitleBoxSx = sx({ condition: alca <= 0, sx: activeBoxTitleStyles });
+    const columnTwoTitleBoxSx = sx({ condition: alca > 0, sx: activeBoxTitleStyles });
 
     // Title Styles
-    const titleLabelStyles = {
-        borderRadius: "2px",
-        px: 1,
-        py: 0.5,
-        mr: 1,
-        fontWeight: "bold",
-    };
     const activeLabelColorStyles = { bgcolor: "dark.main", color: "secondary.main" };
     const inactiveLabelColorStyles = { bgcolor: "secondary.darkText", color: "dark.main" };
-    const columnOneTitleSx = sx(titleLabelStyles, inactiveLabelColorStyles, {
+    const columnOneTitleSx = sx(inactiveLabelColorStyles, {
         condition: alca <= 0,
         sx: activeLabelColorStyles,
     });
-    const columnTwoTitleSx = sx(titleLabelStyles, inactiveLabelColorStyles, {
+    const columnTwoTitleSx = sx(inactiveLabelColorStyles, {
         condition: alca > 0,
         sx: activeLabelColorStyles,
     });
@@ -54,10 +46,6 @@ export function Transactions() {
             ${theme.palette.dark.elevation12} 0%,
             ${theme.palette.dark.elevation12} 100%
         ), ${theme.palette.dark.main}`,
-        borderRadius: "4px",
-        boxShadow: 10,
-        p: 2,
-        flex: 1,
     };
     const inactiveBoxStyles = {
         background: `linear-gradient(
@@ -65,9 +53,6 @@ export function Transactions() {
             ${theme.palette.dark.elevation1} 0%,
             ${theme.palette.dark.elevation1} 100%
         ),${theme.palette.dark.main} `,
-        borderRadius: "4px",
-        p: 2,
-        flex: 1,
     };
     const columnOneBoxSx = sx(inactiveBoxStyles, { condition: alca <= 0, sx: activeBoxStyles });
     const columnTwoBoxSx = sx(inactiveBoxStyles, { condition: alca > 0, sx: activeBoxStyles });
@@ -78,11 +63,6 @@ export function Transactions() {
             ${theme.palette.light.elevation3} 100%,
             ${theme.palette.dark.elevation1} 100%
         ), ${theme.palette.dark.main}`,
-        alignItems: "center",
-        borderRadius: "2px",
-        py: 1,
-        mt: 2,
-        mb: 4,
     };
 
     // Common Styles
@@ -91,10 +71,6 @@ export function Transactions() {
     const columnOneFadeOutTxtSx = sx(inactiveFadeOutTextStyle, { condition: alca <= 0, sx: activeFadeOutTextStyle });
     const columnTwoFadeOutTxtSx = sx(inactiveFadeOutTextStyle, { condition: alca > 0, sx: activeFadeOutTextStyle });
 
-    const flexCenterSx = sx({ display: "flex", alignItems: "center" });
-    const flexColumnSx = sx({ display: "flex", flexDirection: "column" });
-    const resetActionSx = sx({ mt: 2, display: "flex", justifyContent: "flex-end" });
-
     return (
         <>
             <NavigationBar />
@@ -102,18 +78,26 @@ export function Transactions() {
             <Container maxWidth="lg">
                 <SubNavigation />
 
-                <Grid container sx={{ mt: 4 }}>
-                    <Grid item xs={4} sx={flexColumnSx}>
-                        <Box sx={columnOneTitleBoxSx}>
+                <Grid container>
+                    <Grid item xs={4} display="flex" flexDirection="column">
+                        <Box p={2} borderRadius="4px" sx={columnOneTitleBoxSx}>
                             <Typography variant="body1" component="h1">
-                                <Box component="span" sx={columnOneTitleSx}>
+                                <Box
+                                    component="span"
+                                    px={1}
+                                    py={0.5}
+                                    mr={1}
+                                    borderRadius="2px"
+                                    fontWeight="bold"
+                                    sx={columnOneTitleSx}
+                                >
                                     1
                                 </Box>
                                 Migration {symbols.MAD} to {symbols.ALCA}
                             </Typography>
                         </Box>
 
-                        <Box p={2} sx={columnOneBoxSx}>
+                        <Box p={2} borderRadius="4px" flex={1} sx={columnOneBoxSx}>
                             <Typography sx={columnOneFadeOutTxtSx}>Current {symbols.MAD} Balance</Typography>
                             <Typography variant="h5">
                                 {balances.mad || 0} {symbols.MAD}
@@ -128,7 +112,7 @@ export function Transactions() {
                                 1 {symbols.MAD} Token ≈ 1.56 {symbols.ALCA} Token
                             </Typography>
 
-                            <Box sx={[flexCenterSx, { columnGap: 1, mt: 3, mb: 1 }]}>
+                            <Box columnGap={1} mt={3} mb={1} display="flex" alignItems="center">
                                 <TextField
                                     id=""
                                     label="Migrate to ALCA"
@@ -150,59 +134,75 @@ export function Transactions() {
                         </Box>
                     </Grid>
 
-                    <Grid item xs={8} sx={flexColumnSx}>
-                        <Box sx={columnTwoTitleBoxSx}>
+                    <Grid item xs={8} display="flex" flexDirection="column">
+                        <Box p={2} borderRadius="4px" sx={columnTwoTitleBoxSx}>
                             <Typography variant="body1" component="h1">
-                                <Box component="span" sx={columnTwoTitleSx}>
+                                <Box
+                                    component="span"
+                                    px={1}
+                                    py={0.5}
+                                    mr={1}
+                                    borderRadius="2px"
+                                    fontWeight="bold"
+                                    sx={columnTwoTitleSx}
+                                >
                                     2
                                 </Box>
                                 Staking & Lockup {symbols.ALCA}
                             </Typography>
                         </Box>
 
-                        <Box sx={columnTwoBoxSx}>
-                            <Typography sx={[{ fontSize: "14px" }]}>Future {symbols.ALCA} balance</Typography>
+                        <Box p={2} borderRadius="4px" flex={1} sx={columnTwoBoxSx}>
+                            <Typography>Future {symbols.ALCA} balance</Typography>
                             <Typography variant="h5">
                                 {alca || 0} {symbols.ALCA}
                             </Typography>
 
                             <Divider sx={{ my: 2 }} />
 
-                            <Typography sx={[{ fontSize: "14px" }]}>CREATE STAKING</Typography>
-
-                            <Grid sx={[{ alignItems: "center", mt: 2 }]}>
+                            <Typography>CREATE STAKING</Typography>
+                            <Grid mt={2} alignItems="center">
                                 <Grid container item>
                                     <Grid item xs={5}>
-                                        <Typography sx={[{ px: 2 }, flexCenterSx]}>
+                                        <Typography px={2} display="flex" alignItems="center">
                                             Amount
                                             <InfoOutlined color="disabled" fontSize="small" sx={{ ml: 0.5 }} />
                                         </Typography>
                                     </Grid>
 
                                     <Grid item xs>
-                                        <Typography sx={flexCenterSx}>
+                                        <Typography display="flex" alignItems="center">
                                             Reward Type
                                             <InfoOutlined color="disabled" fontSize="small" sx={{ ml: 0.5 }} />
                                         </Typography>
                                     </Grid>
 
                                     <Grid item xs>
-                                        <Typography sx={flexCenterSx}>
+                                        <Typography display="flex" alignItems="center">
                                             Rewards
                                             <InfoOutlined color="disabled" fontSize="small" sx={{ ml: 0.5 }} />
                                         </Typography>
                                     </Grid>
 
-                                    <Grid item xs>
-                                        <Typography sx={flexCenterSx}>
+                                    <Grid item xs columnGap={1}>
+                                        <Typography display="flex" alignItems="center">
                                             Lockup
                                             <InfoOutlined color="disabled" fontSize="small" sx={{ ml: 0.5 }} />
                                         </Typography>
                                     </Grid>
                                 </Grid>
 
-                                <Grid container item sx={[gridStyles]}>
-                                    <Grid item xs={5} sx={[{ px: 2, columnGap: 1 }, flexCenterSx]}>
+                                <Grid
+                                    container
+                                    item
+                                    alignItems="center"
+                                    borderRadius="2px"
+                                    py={1}
+                                    mt={2}
+                                    mb={4}
+                                    sx={[gridStyles]}
+                                >
+                                    <Grid item xs={5} px={2} columnGap={1} display="flex" alignItems="center">
                                         <TextField
                                             id=""
                                             value={0}
@@ -239,10 +239,11 @@ export function Transactions() {
                     </Grid>
                 </Grid>
 
-                <Box sx={resetActionSx}>
-                    <Button sx={[{ mr: 1 }]} variant="outlined" disabled={!alca}>
+                <Box columnGap={1} mt={2} display="flex" justifyContent="flex-end">
+                    <Button variant="outlined" disabled={!alca}>
                         Reset TX
                     </Button>
+
                     <Button endIcon={<ChevronRight />} variant="contained" disabled={!alca}>
                         Review TX
                     </Button>
