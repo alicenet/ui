cc17a0706e0f57fae34de7f21a3ba502a46dbbed
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2023-01-20 10:54:00 -0500

    feat: init_tt

---
+++ hw-tictac/src/components/Copyright.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography, Link } from "@mui/material";
+import { configuration } from "config";
+
+export function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://mui.com/">
+                {configuration.site.copyriteName}
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
